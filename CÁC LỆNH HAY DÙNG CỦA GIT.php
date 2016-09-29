6f6475cdeb03194d2d9f88074e7c95737c6aeea6
Minh Anh <minhanh.itqn@gmail.com>
2016-09-29 14:26:10 +0700

    lan dau tien2

---
+++ CÁC LỆNH HAY DÙNG CỦA GIT.php
@@ -0,0 +1,5 @@
+git add . // đẩy lên tất cả
+git commit -m "" // tiêu đề muốn pull lênh
+git tag Name_tag muốn đẩy lên
+git push origin master // đẩy lên nhánh master
+git push origin Name_tag // đẩy lên tag muốn đưa lên
