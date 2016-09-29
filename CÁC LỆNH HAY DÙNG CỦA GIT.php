e77147377f52269899927f57c7796f80935c89cb
Minh Anh <minhanh.itqn@gmail.com>
2016-09-29 15:22:08 +0700

    lan4

--- CÁC LỆNH HAY DÙNG CỦA GIT.php
+++ CÁC LỆNH HAY DÙNG CỦA GIT.php
@@ -1,5 +1,11 @@
 git add . // đẩy lên tất cả
+git add * // đẩy lên tất cả
 git commit -m "" // tiêu đề muốn pull lênh
 git tag Name_tag muốn đẩy lên
 git push origin master // đẩy lên nhánh master
 git push origin Name_tag // đẩy lên tag muốn đưa lên
+git init // tạo repostory trên git
+git checkout -b feature_x  // tạo nhánh mới trên git
+git checkout master // trở lại nhánh 
+git branch -d feature_x // xóa nhánh
+git branch // kiểm tra xem mình đang ở nhánh nào
